882a17b55616736e4150f88dc96640f17fcc4a51
y-lubuntu <yurik.lyulko@gmail.com>
2023-10-30 09:10:33 +0200

    italic author signature. added gitignore.

--- main.py
+++ main.py
@@ -3,28 +3,25 @@ import shutil
 
 import telebot
 from make_mp3 import get_mp3
-
-
-token = '1549377862:AAGoupqWih_2TpUut9GkbbGYxmVmemrn5FA'
+import tokens_and_ids
 
 
 print("started")
 
-bot = telebot.TeleBot(token)
-chat_id = -1001555273110
+bot = telebot.TeleBot(tokens_and_ids.token)
+chat_id = tokens_and_ids.chat_id
 
 
-#   dsfg channel id             -1002041935041
-#   anything else channel id    -1001555273110
 @bot.channel_post_handler(content_types=['text'])
 def channel(message):
-
+    # memberobject = bot.get_chat_member(chat_id, message.from.user.id)
+    print(message)
     if message.chat.id == chat_id:
         mp3_info = get_mp3(message.text)
         audio_file = open(mp3_info['mp3_path'], "rb")
         bot.delete_message(chat_id=chat_id, message_id=message.id)
         bot.send_audio(message.chat.id, audio_file,
-                       caption=f'[anything else](https://t.me/else_anything) | [ytm]({mp3_info["video_url"]}) | {message.author_signature}',
+                       caption=f'[anything else](https://t.me/else_anything) | [ytm]({mp3_info["video_url"]}) | _{message.author_signature}_',
                        parse_mode='Markdown')
         audio_file.close()
 
