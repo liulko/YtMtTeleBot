0e8c7dda9c56ffc3b03dd9687bec77d5039a352a
Yurii Liulko <yurik.lyulko@gmail.com>
2023-10-30 09:29:56 +0200

    remove unnecessary print

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ chat_id = tokens_and_ids.chat_id
 @bot.channel_post_handler(content_types=['text'])
 def channel(message):
     # memberobject = bot.get_chat_member(chat_id, message.from.user.id)
-    print(message)
+    # print(message)
     if message.chat.id == chat_id:
         mp3_info = get_mp3(message.text)
         audio_file = open(mp3_info['mp3_path'], "rb")
